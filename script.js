c1a39079732ebff1be21ce587b8cfdbbe9db1087
sahalkk <msahalkk357@gmail.com>
2022-10-08 13:51:55 +0530

    script

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 let count = 0;
+
+const value = document.querySelector('#value');
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains("inc")) {
+            count--;
+        }
+        else if (styles.contains("dec")) {
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        value.textContent = count;
+        console.log(e.currentTarget.classList);
+    });
+
+});
